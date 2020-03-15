bf58e008f8a475b763a048ddee4e349f4398e807
Dewmal <dewmalnilanka@gmail.com>
2020-03-16 04:32:43 +0530

    Store Location update

---
+++ android/app/src/main/kotlin/app/ceylon/selftrackingapp/dao/LocationDao.kt
@@ -0,0 +1,19 @@
+package app.ceylon.selftrackingapp.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import app.ceylon.selftrackingapp.model.LocationModel
+import java.util.*
+
+@Dao
+interface LocationDao {
+    @Query("SELECT * FROM locationmodel")
+    fun getAll(): List<LocationModel>
+
+    @Query("SELECT * FROM locationmodel WHERE date BETWEEN :startDate and :endDate")
+    fun getAllByDateRange(startDate: Date, endDate: Date): List<LocationModel>
+
+    @Insert
+    fun insert(vararg locations: LocationModel)
+}
---
+++ android/app/src/main/kotlin/app/ceylon/selftrackingapp/service/LocationTrackingService.kt
@@ -0,0 +1,16 @@
+package app.ceylon.selftrackingapp.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+
+class LocationTrackingService : Service() {
+
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+
+}
