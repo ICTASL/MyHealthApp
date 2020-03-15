a6a98ade8db8ee1e635851e883078c1efd0a992c
Fahim Farook <fahimf@gmail.com>
2020-03-15 15:33:11 +0530

    Basic Registration Screen
    * Added basic registration screen with the ability to add more fields pretty easily
    * Added mechanism to access all input values when "Register" button is clicked (still need to write the API client for creating the user record via the DHIS2 API and submitting the data to the server)

--- lib/page/screen/user_register_screen.dart
+++ lib/page/screen/user_register_screen.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
 
 class UserRegisterScreen extends StatelessWidget {
+  final List<String> items = <String>['Name', 'E-mail', 'Mobile Number'];
+  final List<TextEditingController> edit = <TextEditingController>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("User Registration"),
       ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("User Register Screen"),
-        ],
-      )),
+      body: ListView.separated(
+          padding: EdgeInsets.all(8),
+          itemBuilder: (BuildContext context, int index) {
+            if (index == items.length) {
+              return RaisedButton(
+                child: Text("Register"),
+                color: Colors.blue,
+                textColor: Colors.white,
+                onPressed: () {
+                  // Submit to DHIS API
+                  for (var i = 0; i < edit.length; i++) {
+                    // Get field value
+                    String value = edit[i].text;
+                    print(value);
+                  }
+                },
+              );
+            }
+            TextEditingController ec = TextEditingController();
+            edit.add(ec);
+            return TextField(
+              controller: ec,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: items[index],
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 16,
+            );
+          },
+          itemCount: items.length + 1),
     );
   }
 }
