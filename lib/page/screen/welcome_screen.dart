0028635071108e98a130fe52acfd316da9b67d87
Dewmal <dewmalnilanka@gmail.com>
2020-03-15 10:47:22 +0530

    Welcome page add basic styles

--- lib/page/screen/welcome_screen.dart
+++ lib/page/screen/welcome_screen.dart
@@ -1,34 +1,96 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:selftrackingapp/page/routes.dart';
 import 'package:selftrackingapp/page/screen/dashboard_screen.dart';
 import 'package:selftrackingapp/page/screen/privacy_policy_screen.dart';
+import 'package:selftrackingapp/theme.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("Welcome Screen weda neda"),
-          FlatButton(
-            color: Colors.amber,
-            child: Text("Home Screen"),
-            onPressed: () {
-              Navigator.push(context, createRoute(DashboardScreen()));
-            },
-          ),
-          FlatButton(
-            child: Text("Privacy Policy Screen"),
-            onPressed: () {
-              Navigator.push(context, createRoute(PrivacyPolicyScreen()));
-            },
-          )
-        ],
-      )),
-    );
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+            value: SystemUiOverlayStyle.light,
+            child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.1, 0.4, 0.7, 0.9],
+                    colors: [
+                      Color(0xFF181C30),
+                      Color(0xFF181C30),
+                      Color(0xFF181C30),
+                      Color(0xFF181C30),
+                    ],
+                  ),
+                ),
+                child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 1.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Container(
+                            decoration:
+                                BoxDecoration(color: colorAccentBackground),
+                            child: Image.asset(
+                              "assets/images/welcome_screen_bg.png",
+                              width: 300,
+                              height: 300,
+                            ),
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 20.0, vertical: 20.0),
+                                child: Container(
+                                  child: Text(
+                                    "Welcome",
+                                    style: h1TextStyle.copyWith(
+                                        fontWeight: FontWeight.w600,
+                                        color: mainButtonTextColor),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 20.0, vertical: 5.0),
+                                child: Container(
+                                  child: Text(
+                                    "Together we can defeat COVID-19",
+                                    style: h4TextStyle.copyWith(
+                                        fontWeight: FontWeight.w300,
+                                        color: mainButtonTextColor),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 20.0, vertical: 20.0),
+                                child: Container(
+                                  child: FlatButton(
+                                    color: Color(0XFF8DC63F),
+                                    child: Padding(
+                                      padding: EdgeInsets.all(16.0),
+                                      child: Text(
+                                        "Next",
+                                        style: h2TextStyle.copyWith(
+                                            color: textColor,
+                                            fontWeight: FontWeight.w400),
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.push(context,
+                                          createRoute(DashboardScreen()));
+                                    },
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        ])))));
   }
 }
---
+++ lib/theme.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+const primaryColorSwitch = Color(0xFF615ed8);
+const colorBackground = Color(0xFF242424);
+const colorAccentBackground = Color(0xFFFFFFFF);
+const colorBackgroundItemBorder = Color(0xFF707070);
+const colorDefaultCardBackground = Color(0xFF615ed8);
+const colorOne = Color(0xFF951288);
+
+var backgroundShadowColor = Colors.black.withOpacity(0.48);
+
+var titleBackgroundColor = colorOne;
+
+const textColor = Color(0xFFFFFFFF);
+const textLinkColor = Colors.white;
+const textUnderLineColor = Color(0xFFFFFFFF);
+const textSecondColor = Color(0xFFf9f871);
+
+const defaultButtonColor = Color(0xFFff8e6f);
+const defaultButtonTextColor = Color(0xFFFFFFFF);
+
+const warningButtonColor = Color(0xFFEF7801);
+const warningButtonTextColor = Color(0xFFFFFFFF);
+const warningButtonFlipTextColor = Color(0xFFEF7801);
+
+const mainButtonColor = Color(0xFFE849F3);
+const mainButtonTextColor = Color(0xFFFFFFFF);
+
+const successButtonColor = Color(0xFF7C28C7);
+const successButtonTextColor = Color(0xFFFFFFFF);
+const successButtonFlipTextColor = Color(0xFF7C28C7);
+
+// Text StyleElement
+const h1TextStyle = TextStyle(fontSize: 34.0, color: textColor);
+const h2TextStyle = TextStyle(fontSize: 24.0, color: textColor);
+const h3TextStyle = TextStyle(fontSize: 20.0, color: textColor);
+const h4TextStyle = TextStyle(fontSize: 18.0, color: textColor);
+const h5TextStyle = TextStyle(fontSize: 16.0, color: textColor);
+const h6TextStyle = TextStyle(fontSize: 14.0, color: textColor);
+
+var linkButtonTextStyle = h5TextStyle.copyWith(color: Colors.white);
+
+var backgroundTextShadow = Shadow(
+  blurRadius: 10.0,
+  color: backgroundShadowColor,
+  offset: Offset(5.0, 10.0),
+);
+var backgroundBoxShadow = BoxShadow(
+  blurRadius: 10.0,
+  color: backgroundShadowColor,
+  offset: Offset(5.0, 10.0),
+);
