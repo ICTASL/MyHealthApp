84a73a491c5db3f66161d9c5da72806a9913ba8f
Fahim Farook <fahimf@gmail.com>
2020-03-16 07:45:26 +0530

    Case Details Screen - Map
    * Set up support for displaying map view on Android and iOS
    * Changed text for location permission request on iOS
    * Added basic map view to Case Details screen

--- lib/page/screen/case_details_screen.dart
+++ lib/page/screen/case_details_screen.dart
@@ -1,33 +1,35 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:selftrackingapp/models/location.dart';
 import 'package:selftrackingapp/app_localizations.dart';
-import 'package:selftrackingapp/page/screen/user_register_screen.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class CaseDetailScreen extends StatelessWidget {
+class CaseDetailScreen extends StatefulWidget {
+  @override
+  State<CaseDetailScreen> createState() => CaseDetailScreenState();
+}
+
+class CaseDetailScreenState extends State<CaseDetailScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
       appBar: AppBar(
           title: Text(
         AppLocalizations.of(context).translate('case_details_screen_title'),
       )),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(AppLocalizations.of(context)
-              .translate('case_details_screen_title')),
-          FlatButton(
-            color: Colors.blue,
-            child: Text("User register"),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => UserRegisterScreen()));
-            },
-          )
-        ],
-      )),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(6.9271, 79.8612),
+          zoom: 10,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
