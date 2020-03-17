8b3eb321600df309c550787ed674e1fd1a77043c
Nirmal Ariyathilake <nirmalnavitas@gmail.com>
2020-03-17 21:03:18 +0530

    Screens : Contact Us & User Register

--- lib/page/screen/user_register_screen.dart
+++ lib/page/screen/user_register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:selftrackingapp/app_localizations.dart';
+import 'package:selftrackingapp/utils/tracker_colors.dart';
 
 class UserRegisterScreen extends StatelessWidget {
   final List<String> items = <String>[
@@ -11,12 +12,12 @@ class UserRegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context)
-            .translate('user_register_screen_title')),
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 8.0,
+        vertical: 10.0
       ),
-      body: ListView.separated(
+      child: ListView.separated(
           padding: EdgeInsets.all(8),
           itemBuilder: (BuildContext context, int index) {
             if (index == items.length) {
@@ -25,8 +26,11 @@ class UserRegisterScreen extends StatelessWidget {
                   AppLocalizations.of(context)
                       .translate('user_register_screen_register'),
                 ),
-                color: Colors.blue,
+                color: Color(TrackerColors.primaryColor),
                 textColor: Colors.white,
+                padding: EdgeInsets.symmetric(
+                  vertical: 12.0
+                ),
                 onPressed: () {
                   // Submit to DHIS API
                   for (var i = 0; i < edit.length; i++) {
@@ -42,7 +46,15 @@ class UserRegisterScreen extends StatelessWidget {
             return TextField(
               controller: ec,
               decoration: InputDecoration(
+                hintStyle: TextStyle(color: Color(TrackerColors.primaryColor)),
+                labelStyle: TextStyle(color: Color(TrackerColors.primaryColor)),
                 border: OutlineInputBorder(),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.grey, width: 0.0),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.grey, width: 0.0),
+                ),
                 labelText: AppLocalizations.of(context).translate(items[index]),
               ),
             );
