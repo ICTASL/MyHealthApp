0a8bbec8f7f0235e2dd52f929e41210f0110def1
Nirmal Ariyathilake <nirmalnavitas@gmail.com>
2020-03-19 15:15:59 +0530

    Screen : Root : App Name, Tabs order,icons & names

--- lib/page/screen/root_screen.dart
+++ lib/page/screen/root_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:package_info/package_info.dart';
 import 'package:provider/provider.dart';
 import 'package:selftrackingapp/models/news_article.dart';
 import 'package:selftrackingapp/networking/api_client.dart';
@@ -31,6 +32,10 @@ class _RootScreenState extends State<RootScreen> {
   final FirebaseMessaging _messaging = FirebaseMessaging();
   final StoriesModel _storiesModel = StoriesModel();
 
+  String _appName = "Sri Lanka COVID-19";
+  String _version = "1.0.0";
+
+
   int _currentIndex = 0;
   final _homeTabs = {
     DashboardScreen(),
@@ -38,7 +43,7 @@ class _RootScreenState extends State<RootScreen> {
     CaseDetailScreen(),
     ContactUsScreen(),
   };
-  
+
   List<TitledNavigationBarItem> _homeTabItems;
 
   @override
@@ -46,6 +51,11 @@ class _RootScreenState extends State<RootScreen> {
     super.initState();
     _configureFCM();
     _messaging.subscribeToTopic("mobile_message");
+
+    PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
+      _appName = packageInfo.appName;
+      _version = packageInfo.version;
+    });
   }
 
   void _handleFCM(Map<String, dynamic> message) async {
@@ -84,40 +94,64 @@ class _RootScreenState extends State<RootScreen> {
   @override
   Widget build(BuildContext context) {
     _homeTabItems = [
-      TitledNavigationBarItem(title: AppLocalizations.of(context).translate('dashboard_home_tab_text'), icon: Icons.home),
-      TitledNavigationBarItem(title: AppLocalizations.of(context).translate('dashboard_location_tab_text'), icon: Icons.map),
-      TitledNavigationBarItem(title: AppLocalizations.of(context).translate('dashboard_contact_tab_text'), icon: Icons.phone),
-      TitledNavigationBarItem(title: AppLocalizations.of(context).translate('dashboard_register_tab_text'), icon: Icons.person_add),
+      TitledNavigationBarItem(
+          title:
+              AppLocalizations.of(context).translate('dashboard_home_tab_text'),
+          icon: Icons.home),
+      TitledNavigationBarItem(
+          title: AppLocalizations.of(context)
+              .translate('dashboard_screen_case_list_button'),
+          icon: Icons.location_searching),
+      TitledNavigationBarItem(
+          title: AppLocalizations.of(context)
+              .translate('dashboard_safe_track_tab_text'),
+          icon: Icons.map),
+      TitledNavigationBarItem(
+          title: AppLocalizations.of(context)
+              .translate('dashboard_contact_tab_text'),
+          icon: Icons.phone),
     ];
+
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Colors.white,
           title: Text(
-            _homeTabItems[_currentIndex].title,
+            _appName,
             style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.bold,
                 fontSize: 20.0),
           )),
-      body: MultiProvider(
-        providers: [
-          ChangeNotifierProvider<RegisteredCasesModel>(
-              create: (context) => RegisteredCasesModel()),
-          ChangeNotifierProvider<StoriesModel>(
-              create: (context) => _storiesModel),
-        ],
-        child: _homeTabs.elementAt(_currentIndex),
+      body: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Text(
+              _homeTabItems[_currentIndex].title,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0),
+            )),
+        body: MultiProvider(
+          providers: [
+            ChangeNotifierProvider<RegisteredCasesModel>(
+                create: (context) => RegisteredCasesModel()),
+            ChangeNotifierProvider<StoriesModel>(
+                create: (context) => _storiesModel),
+          ],
+          child: _homeTabs.elementAt(_currentIndex),
+        ),
+        bottomNavigationBar: TitledBottomNavigationBar(
+            currentIndex:
+                _currentIndex, // Use this to update the Bar giving a position
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            activeColor: TrackerColors.primaryColor,
+            items: _homeTabItems),
       ),
-      bottomNavigationBar: TitledBottomNavigationBar(
-          currentIndex:
-              _currentIndex, // Use this to update the Bar giving a position
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          activeColor: TrackerColors.primaryColor,
-          items: _homeTabItems),
     );
   }
 }
