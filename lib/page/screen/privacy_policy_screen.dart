330690030faab7127a7efab3cf8a77780acf8da2
Chathura De Silva <chathurashan89@gmail.com>
2020-03-22 12:04:44 +0530

    changes in privacy policy and faq

--- lib/page/screen/privacy_policy_screen.dart
+++ lib/page/screen/privacy_policy_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:selftrackingapp/networking/data_repository.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class PrivacyPolicyScreen extends StatelessWidget {
   @override
@@ -14,37 +13,42 @@ class PrivacyPolicyScreen extends StatelessWidget {
             style: TextStyle(color: Colors.black),
           ),
         ),
-        body: Container(
-          child: FutureBuilder(
-            future: GetIt.instance<DataRepository>().fetchPrivacyPolicy(),
-            builder: (context, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                  return Center(
-                    child:
-                        Text("Failed getting privacy policy, try again later"),
-                  );
-                  break;
-                case ConnectionState.waiting:
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                  break;
-                case ConnectionState.active:
-                  return Center(
-                    child:
-                        Text("Failed getting privacy policy, try again later"),
-                  );
-                  break;
-                case ConnectionState.done:
-                  return Container(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text(snapshot.data),
-                  );
-                  break;
-              }
-            },
-          ),
+        body: WebView(
+          initialUrl: "http://covid19.egreen.io/privacy_policy.html",
+          javascriptMode: JavascriptMode.unrestricted,
         ));
   }
 }
+
+//Container(
+//          child: FutureBuilder(
+//            future: GetIt.instance<DataRepository>().fetchPrivacyPolicy(),
+//            builder: (context, snapshot) {
+//              switch (snapshot.connectionState) {
+//                case ConnectionState.none:
+//                  return Center(
+//                    child:
+//                        Text("Failed getting privacy policy, try again later"),
+//                  );
+//                  break;
+//                case ConnectionState.waiting:
+//                  return Center(
+//                    child: CircularProgressIndicator(),
+//                  );
+//                  break;
+//                case ConnectionState.active:
+//                  return Center(
+//                    child:
+//                        Text("Failed getting privacy policy, try again later"),
+//                  );
+//                  break;
+//                case ConnectionState.done:
+//                  return Container(
+//                    padding: const EdgeInsets.all(20.0),
+//                    child: Text(snapshot.data),
+//                  );
+//                  break;
+//              }
+//            },
+//          ),
+//        )
