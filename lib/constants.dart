1ecc75bd5bfb918deb610e4776b45247a7a846de
Dewmal <dewmalnilanka@gmail.com>
2020-04-07 20:05:17 +0530

    v1.11.0 release build

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
-final bool debugRelease = true;
+final bool debugRelease = false;
 //final String testingServer = "https://test.covid-19.health.gov.lk/api";
 final String testingServer = "https://test.covid-19.health.gov.lk/api";
