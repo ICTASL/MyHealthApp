bf110e55e2b60a0fa223c226938b50ead554687a
Ali Ahamed Thowfeek <alitheoneman@gmail.com>
2020-04-10 22:13:21 +0530

    made the whole card tappable

--- lib/page/screen/contact_us_screen.dart
+++ lib/page/screen/contact_us_screen.dart
@@ -128,189 +128,182 @@ class _ContactUsScreenState extends State<ContactUsScreen> {
       List<ContactUsContact> subContacts,
       bool subtitleTranslate,
       bool titleTranslate) {
-    return Card(
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20.0))),
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
+    return GestureDetector(
+      onTap: () async {
+        if (subContacts == null && await canLaunch(link)) {
+          await launch(link);
+        } else if (subContacts != null) {
+          await showDialog(
+            barrierDismissible: true,
+            context: contextParent,
+            child: AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              title: Container(
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        AppLocalizations.of(context)
+                            .translate("medical_consultation_service_title"),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25.0),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        AppLocalizations.of(context)
+                            .translate("medical_consultation_service_foc"),
+                        style: TextStyle(fontSize: 14.0, color: Colors.grey),
+                        textAlign: TextAlign.center,
+                      ),
+                      Divider(),
+                    ],
+                  ),
+                ),
+              ),
+              contentPadding: const EdgeInsets.all(0.0),
+              content: Container(
+                width: 500,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0))),
+                        color: TrackerColors.primaryColor,
+                        onPressed: () async {
+                          await launch(subContacts[index]
+                              .link
+                              .split(",")[Platform.isIOS ? 1 : 0]);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(
+                            subContacts[index].title,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: subContacts.length,
+                ),
+              ),
+              actions: [
+                FlatButton(
+                    child: Text("Close"),
+                    onPressed: () =>
+                        Navigator.of(context, rootNavigator: true).pop()),
+              ],
+            ),
+          );
+        } else {
+          await showDialog(
+            barrierDismissible: true,
+            context: contextParent,
+            child: AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              title: Text("Oops, something went wrong"),
+              content: Text("Failed to make phone call, try again later."),
+              actions: [
+                FlatButton(
+                    child: Text("Ok"),
+                    onPressed: () => Navigator.of(context).pop()),
+              ],
+            ),
+          );
+        }
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(20.0))),
-        padding: const EdgeInsets.all(20.0),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              constraints: BoxConstraints(
-                  maxWidth: MediaQuery.of(context).size.width / 2),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(
-                    !titleTranslate
-                        ? title
-                        : AppLocalizations.of(context).translate(title),
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.start,
-                  ),
-                  Text(
-                    !subtitleTranslate
-                        ? subTitle
-                        : AppLocalizations.of(context).translate(subTitle),
-                    style: TextStyle(
-                        fontSize: 17.0,
-                        color: Colors.black.withOpacity(0.5),
-                        fontWeight: FontWeight.w400),
-                    textAlign: TextAlign.start,
-                  ),
-                  SizedBox(height: 5.0),
-                  address.isNotEmpty
-                      ? Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            SizedBox(
-                              height: 2.0,
-                            ),
-                            Text(
-                              address,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 13.0,
-                                  fontWeight: FontWeight.w400),
-                              textAlign: TextAlign.start,
-                            ),
-                          ],
-                        )
-                      : Container(),
-                ],
-              ),
-            ),
-            Container(
-              child: GestureDetector(
-                onTap: () async {
-                  if (subContacts == null && await canLaunch(link)) {
-                    await launch(link);
-                  } else if (subContacts != null) {
-                    await showDialog(
-                      barrierDismissible: true,
-                      context: contextParent,
-                      child: AlertDialog(
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20.0))),
-                        title: Container(
-                          child: Center(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  AppLocalizations.of(context).translate(
-                                      "medical_consultation_service_title"),
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 25.0),
-                                ),
-                                SizedBox(
-                                  height: 10.0,
-                                ),
-                                Text(
-                                  AppLocalizations.of(context).translate(
-                                      "medical_consultation_service_foc"),
-                                  style: TextStyle(
-                                      fontSize: 14.0, color: Colors.grey),
-                                  textAlign: TextAlign.center,
-                                ),
-                                Divider(),
-                              ],
-                            ),
-                          ),
-                        ),
-                        contentPadding: const EdgeInsets.all(0.0),
-                        content: Container(
-                          width: 500,
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 10.0, horizontal: 20.0),
-                                child: RaisedButton(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(10.0))),
-                                  color: TrackerColors.primaryColor,
-                                  onPressed: () async {
-                                    await launch(subContacts[index]
-                                        .link
-                                        .split(",")[Platform.isIOS ? 1 : 0]);
-                                  },
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(15.0),
-                                    child: Text(
-                                      subContacts[index].title,
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                            itemCount: subContacts.length,
-                          ),
-                        ),
-                        actions: [
-                          FlatButton(
-                              child: Text("Close"),
-                              onPressed: () =>
-                                  Navigator.of(context, rootNavigator: true)
-                                      .pop()),
-                        ],
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width / 2),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      !titleTranslate
+                          ? title
+                          : AppLocalizations.of(context).translate(title),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
                       ),
-                    );
-                  } else {
-                    await showDialog(
-                      barrierDismissible: true,
-                      context: contextParent,
-                      child: AlertDialog(
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20.0))),
-                        title: Text("Oops, something went wrong"),
-                        content:
-                            Text("Failed to make phone call, try again later."),
-                        actions: [
-                          FlatButton(
-                              child: Text("Ok"),
-                              onPressed: () => Navigator.of(context).pop()),
-                        ],
-                      ),
-                    );
-                  }
-                },
-                child: Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: TrackerColors.primaryColor,
+                      textAlign: TextAlign.start,
                     ),
-                    padding: EdgeInsets.all(15.0),
-                    child: subContacts == null
-                        ? (link.startsWith("tel:")
-                            ? Icon(Icons.phone, color: Colors.white)
-                            : Icon(Icons.web, color: Colors.white))
-                        : Image.asset(
-                            "assets/images/medical_consultion.png",
-                            height: 24,
-                            width: 24,
-                          )),
+                    Text(
+                      !subtitleTranslate
+                          ? subTitle
+                          : AppLocalizations.of(context).translate(subTitle),
+                      style: TextStyle(
+                          fontSize: 17.0,
+                          color: Colors.black.withOpacity(0.5),
+                          fontWeight: FontWeight.w400),
+                      textAlign: TextAlign.start,
+                    ),
+                    SizedBox(height: 5.0),
+                    address.isNotEmpty
+                        ? Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              SizedBox(
+                                height: 2.0,
+                              ),
+                              Text(
+                                address,
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 13.0,
+                                    fontWeight: FontWeight.w400),
+                                textAlign: TextAlign.start,
+                              ),
+                            ],
+                          )
+                        : Container(),
+                  ],
+                ),
               ),
-            ),
-          ],
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: TrackerColors.primaryColor,
+                ),
+                padding: EdgeInsets.all(15.0),
+                child: subContacts == null
+                    ? (link.startsWith("tel:")
+                        ? Icon(Icons.phone, color: Colors.white)
+                        : Icon(Icons.web, color: Colors.white))
+                    : Image.asset(
+                        "assets/images/medical_consultion.png",
+                        height: 24,
+                        width: 24,
+                      ),
+              ),
+            ],
+          ),
         ),
       ),
     );
