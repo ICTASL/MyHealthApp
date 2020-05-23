35da853105d255389a0e0fcc58efccba5dd11e60
Dewmal <dewmalnilanka@gmail.com>
2020-05-23 11:52:42 +0530

    Create Production Release 1.16.0

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
-final bool debugRelease = true;
+final bool debugRelease = false;
 //final String testingServer = "https://test.covid-19.health.gov.lk/api";
 final String testingServer = "https://test.covid-19.health.gov.lk/api";
 
